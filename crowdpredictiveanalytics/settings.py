b744e20cc8f5de4f9d0c188d997be9d2373fc742
Fannie Barskhian <barskhianfannie@gmail.com>
2022-04-20 17:13:30 -0700

    updating phone sizing

--- crowdpredictiveanalytics/settings.py
+++ crowdpredictiveanalytics/settings.py
@@ -24,7 +24,7 @@ DEBUG=True
 SECRET_KEY= 'y2j!+h!gm$e$2l+t=6onxpie)!ppr866f^6g7wrq+k@^^ntt_c'
 ALLOWED_HOSTS=['crowdpredictiveanalytics.com/','crowdpredictiveanalytics.com',"www.crowdpredictiveanalytics.com/","www.crowdpredictiveanalytics.com", "*","www.my-app-pl8tf.ondigitalocean.app/", "my-app-pl8tf.ondigitalocean.app/"]
 DJANGO_ALLOWED_HOSTS = ['crowdpredictiveanalytics.com/','crowdpredictiveanalytics.com',"www.crowdpredictiveanalytics.com/","www.crowdpredictiveanalytics.com", "*","www.my-app-pl8tf.ondigitalocean.app/", "my-app-pl8tf.ondigitalocean.app/"]
-
+# ALLOWED_HOSTS = ['127.0.0.1,localhost']
 
 # Application definition
 
