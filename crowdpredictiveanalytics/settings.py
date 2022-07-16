e5cb6b7ff4ed3a0254c8fc1246882931cfba22ce
Fannie Barskhian <barskhianfannie@gmail.com>
2022-07-15 22:08:01 -0700

    updates dtatic files

--- crowdpredictiveanalytics/settings.py
+++ crowdpredictiveanalytics/settings.py
@@ -184,10 +184,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
-STATICFILES_FINDERS = (
-'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-'django.contrib.staticfiles.finders.FileSystemFinder',
-)
+
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # in production, we want cdn
